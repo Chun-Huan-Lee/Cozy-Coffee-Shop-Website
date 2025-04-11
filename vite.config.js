36ad289822e589fadea05bb96dbf414caa03e278
CHUN-HUAN LEE <vincent741516899@gmail.com>
2025-04-11 14:31:03 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://chun-huan-lee.github.io/Cozy-Coffee-Shop-Website/",
   plugins: [react()],
 })
